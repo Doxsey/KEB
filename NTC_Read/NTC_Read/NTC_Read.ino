204b3c5834da13d45a790c7d64483c869737bd7e
Doxsey <ryan.doxsey@gmail.com>
2015-06-16 08:14:58 -0500

    Initial Timer Interrupt for Sampling

--- NTC_Read/NTC_Read/NTC_Read.ino
+++ NTC_Read/NTC_Read/NTC_Read.ino
@@ -5,6 +5,13 @@ int resistorCase;
 int var;
 int variableDelay;
 float avgTemp;
+boolean toggle1 = 0;
+int sampleCount;
+float constantAvg;
+int constantCount;
+float constantSum;
+float movingAvg;
+float movingSum;
 
 
 double Thermistor(int RawADC) {
@@ -22,61 +29,73 @@ int topFan = 5;
 
 
 void setup() {
+  cli();//stop interrupts
  Serial.begin(115200);
  pinMode(6, OUTPUT);
  resistorCase = 0;
  variableDelay = 1000;
+ sampleCount=0;
+ movingAvg = 0;
+ movingSum = 1;
+ constantAvg = 0;
+constantCount = 0;
+constantSum = 0;
+ avgTemp = 0;
+resistorCase = 4;
+//var = 0;
+//while(var < 40)  
+//  {
+//   avgTemp = avgTemp + float(Thermistor(analogRead(0)));
+//
+//    var++;
+//    delay(5);
+//  }
+//
+//avgTemp = avgTemp / 40;
+// 
+ //set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+ sei();//allow interrupts
 }
 
 void loop() {
 
-avgTemp = 0;
+Display_Data();
+Choose_Case();
+caseStatements();  
+ 
+delay(2000);
+}
 
-var = 0;
-while(var < 40)  
+void Display_Data()
   {
-   avgTemp = avgTemp + float(Thermistor(analogRead(0)));
-
-    var++;
-    delay(5);
+    Serial.print((char)27); // ESC
+    Serial.print("[2J"); // clear screen
+    Serial.print((char)27); // ESC
+    Serial.print("[H"); // cursor to home
+    Serial.print("Current Temp: ");
+    Serial.println(movingAvg);  // display Celcius
+    Serial.print("Case: "); 
+    Serial.println(resistorCase);
+    Serial.print("Running Average: "); 
+    Serial.println(constantAvg);
+    Serial.print("Running Average Data Points: "); 
+    Serial.println(constantCount);
   }
-
-avgTemp = avgTemp / 40;
-
-Serial.print((char)27); // ESC
-Serial.print("[2J"); // clear screen
-Serial.print((char)27); // ESC
-Serial.print("[H"); // cursor to home
-
-Serial.println(avgTemp);  // display Fahrenheit
-//Serial.println(analogRead(0));
-
- 
- 
-  if (avgTemp > 45.5)
-    {
-      resistorCase = 1; //Hotter than 45 degrees
-    }
- 
-  else if (avgTemp == 45)
-    {
-      resistorCase = 2; //Temp is 45
-    }
-  else if (avgTemp < 44)
-    {
-     resistorCase = 3; //Temp less than 44.5
-    }
- 
-  else
-    {
-      resistorCase = 4;
-    }
-    
-Serial.print("Case: "); 
-Serial.println(resistorCase);
   
-  
-  switch (resistorCase) {
+void caseStatements()
+  {
+    switch (resistorCase) {
     case 1:
       digitalWrite(resitorPin, LOW); 
       digitalWrite(topFan, HIGH);
@@ -101,9 +120,51 @@ Serial.println(resistorCase);
       Serial.println("Fan Off, Resistor Off");
       variableDelay = 5000;
       break;
-      
+    }
   }
+
+void Choose_Case()
+  {
+    if (avgTemp > 45.5)
+    {
+      resistorCase = 1; //Hotter than 45 degrees
+    }
+ 
+  else if (avgTemp == 45)
+    {
+      resistorCase = 2; //Temp is 45
+    }
+  else if (avgTemp < 44)
+    {
+     resistorCase = 3; //Temp less than 44.5
+    }
+ 
+  else
+    {
+      resistorCase = 4;
+    }
+  }
+
+ISR(TIMER1_COMPA_vect)  //timer1 interrupt 1Hz toggles pin 13 (LED)
+  {
+    
+    constantSum = constantSum + float(Thermistor(analogRead(0)));
+    constantCount++;
+    constantAvg = constantSum / constantCount;
+    
+  if (sampleCount < 5)
+    {
+      movingSum = movingSum + float(Thermistor(analogRead(0)));
+      sampleCount++;
+    }
+  else if (sampleCount == 5)
+    {
+      movingSum = movingSum + float(Thermistor(analogRead(0)));
+      movingAvg = movingSum / 5;
+      movingSum = 0; 
+      sampleCount = 1;  
+    }
   
-  
-delay(variableDelay);
-}
+  }
+
+
